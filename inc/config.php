c567047162f3c465fab31182395675d730a69758
yasirutomo <yasirutomo@gmail.com>
2016-12-18 05:33:39 +0700

    mysql database config
    mysql database config, using PHP 5.6, change to mysqli for PHP 7.0

---
+++ inc/config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Author : Muhammad Nur Yasir Utomo
+Email : yasirutomo@gmail.com
+*/
+
+	define('db_host','localhost');
+	define('db_user','root');
+	define('db_pass','');
+	define('db_name','gmaps');
+	
+	mysql_connect(db_host,db_user,db_pass);
+	mysql_select_db(db_name);
+	
+?>
